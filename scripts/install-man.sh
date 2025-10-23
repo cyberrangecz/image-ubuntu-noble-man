c33a99ea4e17f9a98fa83e4f9fd6aa28fab0901b
Tomas Sapak <sapak@cshub.cz>
2025-10-23 10:15:36 +0200

    Install guacd from package manager

--- scripts/install-man.sh
+++ scripts/install-man.sh
@@ -3,8 +3,9 @@ sudo apt update
 # install chrony
 sudo apt install -y chrony
 
-# install k3s
-curl -sfL https://get.k3s.io | sudo INSTALL_K3S_VERSION="v1.33.1+k3s1" sh -s -
+# install & configure guacd
+sudo apt install -y guacd
+sudo sed -i 's/^LISTEN_ADDRESS=127\.0\.0\.1$/LISTEN_ADDRESS=0\.0\.0\.0/' /etc/default/guacd
 
 # install iptables-persistent
 sudo apt install -y iptables-persistent
@@ -14,9 +15,3 @@ sudo apt install -y syslog-ng
 
 # install python3-yaml
 sudo apt install -y python3-yaml
-
-# install helm
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-
-# fetch guacd image
-sudo ctr images pull docker.io/guacamole/guacd:1.6.0
